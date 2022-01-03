4fa27e85865d3d744a73f375c7c126388fe2583c
Knarkzel <knarkzel@gmail.com>
2022-01-03 03:30:30 +0100

    init

---
+++ v.mod
@@ -0,0 +1,7 @@
+Module {
+	name: 'vom'
+	description: ''
+	version: '0.0.0'
+	license: 'MIT'
+	dependencies: []
+}
---
+++ vom.v
@@ -0,0 +1,46 @@
+module main
+
+type Parser = fn (string) ?(string, string)
+
+fn tag(pattern string) fn (string) ?(string, string) {
+	return fn [pattern] (input string) ?(string, string) {
+		if input.len < pattern.len {
+			return none
+		}
+		if input[..pattern.len] == pattern {
+			return input[pattern.len..], input[..pattern.len]
+		} else {
+			return none
+		}
+	}
+}
+
+fn take(count int) fn (string) ?(string, string) {
+	return fn [count] (input string) ?(string, string) {
+		if input.len < count {
+			return none
+		} else {
+			return input[count..], input[..count]
+		}
+	}
+}
+
+fn apply(parsers ...fn (string) ?(string, string)) fn (string) ?(string, []string) {
+	return fn [parsers] (input string) ?(string, []string) {
+		mut temp := input
+		mut output := []string{}
+		for parser in parsers {
+			rest, slice := parser(temp) ?
+			output << slice
+			temp = rest
+		}
+		return temp, output
+	}
+}
+
+fn main() {
+	input := 'hello, world'
+	parser := apply(tag('hello'), take(5))
+	_, output := parser(input) ?
+	println(output)
+}
