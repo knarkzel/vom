8c65ee35b82226c8e52bda97ab06795ed5f359cc
Knarkzel <knarkzel@gmail.com>
2022-01-03 03:49:04 +0100

    better errors

--- vom.v
+++ vom.v
@@ -3,12 +3,12 @@ module main
 fn tag(pattern string) fn (string) ?(string, string) {
 	return fn [pattern] (input string) ?(string, string) {
 		if input.len < pattern.len {
-			return none
+			return error('`tag` failed because input `$input` is shorter than pattern `$pattern`')
 		}
 		if input[..pattern.len] == pattern {
 			return input[pattern.len..], input[..pattern.len]
 		} else {
-			return none
+			return error('`tag` failed because `${input[..pattern.len]}` is not equal to pattern `$pattern`')
 		}
 	}
 }
@@ -16,7 +16,7 @@ fn tag(pattern string) fn (string) ?(string, string) {
 fn take(count int) fn (string) ?(string, string) {
 	return fn [count] (input string) ?(string, string) {
 		if input.len < count {
-			return none
+			return error('`take` failed with count `$count` on input `$input`')
 		} else {
 			return input[count..], input[..count]
 		}
