035351ab5d6743676de1d238103eeb2dac499263
Knarkzel <knarkzel@gmail.com>
2022-01-06 01:08:47 +0100

    rename to multi, starting on charater testsc

---
+++ character_test.v
@@ -0,0 +1,8 @@
+module vom
+
+fn test_alpha0() {
+	// parser := is_a('123456789ABCDEF')
+	// rest, output := parser('DEADBEEF and others') ?
+	// assert output == 'DEADBEEF'
+	// assert rest == ' and others'
+}
