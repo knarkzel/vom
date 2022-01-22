33870e4ccd3c1cc5f1d7c91359aacadb7f3c9f72
Knarkzel <knarkzel@gmail.com>
2022-01-22 02:59:38 +0100

    ops, major bug fix

--- vom.v
+++ vom.v
@@ -1,7 +1,7 @@
 module vom
 
 // Parser which returns a single value
-pub type Fn = fn (string) ?(string, T)
+pub type Fn = fn (string) ?(string, string)
 
 // Parser which returns many values
-pub type FnMany = fn (string) ?(string, []T)
+pub type FnMany = fn (string) ?(string, []string)
